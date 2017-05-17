cbfb384b3076ca8314d6bf41d7923187a5588278
IgorTolstov <grtolstov@yahoo.com>
2017-05-16 22:34:07 -0500

    Created a new package

--- sandbox/src/main/java/ru/stqa/pft/sandbox/HelloWorld.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/HelloWorld.java
@@ -1,6 +1,8 @@
+package ru.stqa.pft.sandbox;
+
 public class HelloWorld {
 
 	public static void main (String [] args) {
-		System.out.println("Helloe World!");
+		System.out.println("Hello World!");
 	}
 }
