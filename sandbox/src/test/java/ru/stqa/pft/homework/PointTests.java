be4f52470af265f38d10e7c21d518cbbce1f355b
IgorTolstov <grtolstov@yahoo.com>
2017-05-23 15:24:10 -0500

    update to PointTests class

--- sandbox/src/test/java/ru/stqa/pft/homework/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/homework/PointTests.java
@@ -21,6 +21,9 @@ public class PointTests {
 
     @Test
     public void test2() {
+        Point p1 = new Point(-3, 0.2);
+        Point p2 = new Point(0, -3);
 
+        Assert.assertEquals((int) p1.distanceTo(p2), 4);
     }
 }
