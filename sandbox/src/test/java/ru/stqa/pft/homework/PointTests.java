672606bee4f34668b2f3201d4b023d1354f31459
IgorTolstov <grtolstov@yahoo.com>
2017-05-23 15:23:28 -0500

    Created PointTests class

---
+++ sandbox/src/test/java/ru/stqa/pft/homework/PointTests.java
@@ -0,0 +1,26 @@
+package ru.stqa.pft.homework;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by itolstov on 5/23/2017.
+ */
+
+public class PointTests {
+
+    @Test
+    public void test1() {
+
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(3, 4);
+
+        Assert.assertEquals(p1.distanceTo(p2), 5.0);
+
+    }
+
+    @Test
+    public void test2() {
+
+    }
+}
