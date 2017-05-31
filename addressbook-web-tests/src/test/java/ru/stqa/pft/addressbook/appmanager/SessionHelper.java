72f2b37fa20022c76916e8a50765a3f8fcb0e074
IgorTolstov <grtolstov@yahoo.com>
2017-05-30 19:25:34 -0500

    Created app manager and helper classes

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
@@ -0,0 +1,22 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by SanchezKlauss on 5/30/2017.
+ */
+public class SessionHelper {
+    private FirefoxDriver wd;
+
+    public SessionHelper(FirefoxDriver wd) {
+
+        this.wd = wd;
+    }
+
+    public void login(String username, String password) {
+        wd.findElement(By.name("user")).sendKeys(username);
+        wd.findElement(By.name("pass")).sendKeys(password);
+        wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+    }
+}
